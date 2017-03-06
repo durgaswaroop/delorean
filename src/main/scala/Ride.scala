22b017005c7d921b28f5669947091d96f5cee92e
DurgaSwaroop <durgaswaroop@gmail.com>
2017-03-06 22:54:37 +0530

    Upadate 'Ride' to create a current timeline pointer & a config file
    So doing "delorean ride" will
    - Create .tm/
    - Create .tm/hashes/
    - Create .tm/pitstops/
    - Create .tm/pitstops/current
    - Create .tm/config

--- src/main/scala/Ride.scala
+++ src/main/scala/Ride.scala
@@ -4,8 +4,12 @@ class Ride {
     val deloreanDirectory = new File(".tm/")
     val pitstopDirectory = new File(".tm/pitstops/")
     val hashesDirectory = new File(".tm/hashes/")
+    val currentTimeLine = new File(".tm/pitstops/current")
+    val configFile = new File(".tm/config")
 
     pitstopDirectory.mkdirs
     hashesDirectory.mkdirs
+    currentTimeLine.createNewFile
+    configFile.createNewFile
     println("Delorean repository created. Delorean is ready for a ride")
 }
