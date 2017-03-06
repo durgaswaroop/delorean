b6f77e57894331106e47a1baff9faf1fe84566c7
DurgaSwaroop <durgaswaroop@gmail.com>
2017-03-06 23:43:34 +0530

    Create metadata directory to store pitstop metadata
    We will store information like, 
    Time of Pitstop, Rider, Rider Log etc..

--- src/main/scala/Ride.scala
+++ src/main/scala/Ride.scala
@@ -4,11 +4,13 @@ class Ride {
     val deloreanDirectory = new File(".tm/")
     val pitstopDirectory = new File(".tm/pitstops/")
     val hashesDirectory = new File(".tm/hashes/")
+    val metadataDirectory = new File(".tm/metadata/")
     val currentTimeLine = new File(".tm/pitstops/current")
     val configFile = new File(".tm/config")
 
     pitstopDirectory.mkdirs
     hashesDirectory.mkdirs
+    metadataDirectory.mkdirs
     currentTimeLine.createNewFile
     configFile.createNewFile
     println("Delorean repository created. Delorean is ready for a ride")
