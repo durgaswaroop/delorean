12dca99fa52529cef60b9a509a0ac3b31770ff6a
DurgaSwaroop <durgaswaroop@gmail.com>
2017-03-07 01:05:19 +0530

    Add simple test for Configuration

---
+++ src/test/scala/ConfigurationTest.scala
@@ -0,0 +1,10 @@
+import org.junit.gen5.api.Assertions._
+import org.junit.gen5.api.Test
+
+class ConfigurationTest {
+    @Test
+    def whenConfigurationNotPresent(): Unit = {
+        assertEquals("", Configuration("alakazam"))
+        assertEquals("", Configuration("Erno"))
+    }
+}
