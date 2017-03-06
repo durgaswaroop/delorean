2efafd1713255bf08879fdbc9b2cce5e1439daa9
DurgaSwaroop <durgaswaroop@gmail.com>
2017-03-07 00:08:11 +0530

    Update Configuration file to read only when the config file exists
    For the first time (before doing 'ride'), the file wont exist. So, then we'll just get an empty Map

--- src/main/scala/Configuration.scala
+++ src/main/scala/Configuration.scala
@@ -1,6 +1,10 @@
+import java.io.File
+
 object Configuration {
     val configFile = ".tm/config"
-    val configurationMap: Map[String, String] = FileOps.getFileAsMap(configFile)
+    val configurationMap: Map[String, String] = {
+        if (new File(configFile).exists()) FileOps.getFileAsMap(configFile) else Map.empty
+    }
 
     def apply(key: String): String = configurationMap.getOrElse(key, "")
 }
