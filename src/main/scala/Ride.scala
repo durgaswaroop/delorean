5c0565270c8878a1a5838fbf1c26623536dd0e47
DurgaSwaroop <durgaswaroop@gmail.com>
2017-03-12 09:03:58 +0530

    Now create "indicators" directory as part of 'ride'
    indicators are timeline indicators. 
    They are the names for the timelines like branches in other vcs's.
    The current timeline file is also in the indicators folder now.

--- src/main/scala/Ride.scala
+++ src/main/scala/Ride.scala
@@ -5,12 +5,15 @@ class Ride {
     val pitstopDirectory = new File(".tm/pitstops/")
     val hashesDirectory = new File(".tm/hashes/")
     val metadataDirectory = new File(".tm/metadata/")
-    val currentTimeLine = new File(".tm/pitstops/current")
+    val indicatorsDirectory = new File(".tm/indicators/")
+
     val configFile = new File(".tm/config")
+    val currentTimeLine = new File(".tm/indicators/current")
 
     pitstopDirectory.mkdirs
     hashesDirectory.mkdirs
     metadataDirectory.mkdirs
+    indicatorsDirectory.mkdir
     currentTimeLine.createNewFile
     configFile.createNewFile
     println("Delorean repository created. Delorean is ready for a ride")
