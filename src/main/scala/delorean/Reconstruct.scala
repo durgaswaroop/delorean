e5f7e03417e5d157d9fe5bed575de2a9729d5756
DurgaSwaroop <durgaswaroop@gmail.com>
2017-03-17 13:30:00 +0530

    Add 'Reconstruct' class
    To reconstruct a file based on the given hash. TO be used when resetting to a different pitstop or switching to a new time-line etc. 
    TODO: Right now, the ordering of the lines is getting messed up. Gotta fix that.

---
+++ src/main/scala/delorean/Reconstruct.scala
@@ -0,0 +1,30 @@
+package delorean
+
+import delorean.FileOps.{getFileAsMap, getLinesOfFile, writeToFile}
+
+/**
+  * Class to reconstruct the files based on the hashes.
+  */
+object Reconstruct {
+
+    /**
+      * Reconstructs a file.
+      *
+      * Will take the file hash we want to reconstruct/reset and will look for that hash in the hashes directory.
+      * Once the file is found, it will look at the hashes of individual lines in the file and
+      * gets the corresponding lines from the String pool file
+      *
+      * @param fileHash hash of the file
+      */
+    def file(fileHash: String): Unit = {
+        val lineHashesOfFile: List[String] = getLinesOfFile(s"$HASHES_FOLDER$fileHash")
+        val stringPoolMap = getFileAsMap(STRING_POOL)
+        var reconstructedLines: String = ""
+        lineHashesOfFile.foreach(lineHash ⇒ reconstructedLines += stringPoolMap(lineHash) + "\n")
+        // Store it in to the appropriate file from the file name in the travelogue file
+        // Travelogue file gets updated with every pitstop. So, the hash for a particular hash might not even
+        // exist in it. Instead we should get the filename from the pitstop to which we are trying to reset to.
+        val fileName: String = getFileAsMap(TRAVELOGUE).filter(e ⇒ e._2 == fileHash).keys.head
+        writeToFile(fileName, reconstructedLines)
+    }
+}
