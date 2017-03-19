b02e889f7bf47858c3eb41f7239b5c63d2faa8ce
DurgaSwaroop <durgaswaroop@gmail.com>
2017-03-19 20:52:06 +0530

    All 'add' to handle directories too
    Till now its only possible to add files. Now, we can add directories too. And all the files in those directories will be added.

--- src/main/scala/delorean/commands/Add.scala
+++ src/main/scala/delorean/commands/Add.scala
@@ -1,11 +1,22 @@
 package delorean.commands
 
+import java.io.File
+
+import delorean.FileOps._
 import delorean.Hasher
 
 /**
   * For the command 'add'.
   */
 case class Add(files: List[String]) {
+    println(files)
     val hasher = new Hasher
-    hasher.computeHashOfAddedFiles(files)
+    var allFiles: List[String] = List[String]()
+    // If a directory is added, get all the files of the directory
+    files.foreach(f ⇒
+        if (new File(f).isDirectory) allFiles = allFiles ::: getFilesRecursively(f) else allFiles = f :: allFiles
+    )
+    // In the list only keep the names of the files.So
+    allFiles = allFiles.filterNot(file ⇒ new File(file).isDirectory)
+    hasher.computeHashOfAddedFiles(allFiles)
 }
