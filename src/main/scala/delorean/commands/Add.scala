abeaf9b3470b46fecaf23828f1129fe86708b01c
DurgaSwaroop <durgaswaroop@gmail.com>
2017-03-27 00:39:39 +0530

    Fix 'add' crash on adding a non-existent file
    It used to crash when we add a non-existent file. Now it doesn't and prints a message saying file doesn't exist instead.

--- src/main/scala/delorean/commands/Add.scala
+++ src/main/scala/delorean/commands/Add.scala
@@ -13,13 +13,20 @@ case class Add(files: List[String]) {
     val logger: Logger = Logger.getLogger(this.getClass.getName)
     logger.fine(s"Adding list of files $files.")
     val hasher = new Hasher
-    var allFiles: List[String] = List.empty
+
+    val (realFiles, imaginaryFiles) = files.span(file ⇒ new File(file).exists())
+
+    imaginaryFiles.foreach(file ⇒ println(s"delorean: File $file does not exist"))
+    if (realFiles.isEmpty) System.exit(1)
+
+    var filesToAdd: List[String] = realFiles
+
     // If a directory is added, get all the files of the directory
     files.foreach(f ⇒
-        if (new File(f).isDirectory) allFiles = allFiles ::: getFilesRecursively(f) else allFiles = f :: allFiles
+        if (new File(f).isDirectory) filesToAdd = filesToAdd ::: getFilesRecursively(f) else filesToAdd = f :: filesToAdd
     )
     // In the list only keep the names of the files.So
-    allFiles = allFiles.filterNot(file ⇒ new File(file).isDirectory)
-    logger.fine(s"After resolving all the files to be added (after deleting the directories) are: $allFiles")
-    hasher.computeHashOfAddedFiles(allFiles)
+    filesToAdd = filesToAdd.filterNot(file ⇒ new File(file).isDirectory)
+    logger.fine(s"After resolving all the files to be added (after deleting the directories) are: $filesToAdd")
+    hasher.computeHashOfAddedFiles(filesToAdd)
 }
