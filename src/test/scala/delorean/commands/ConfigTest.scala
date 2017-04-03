0250a82e1aa17a501e04116cf31393e32f73a030
DurgaSwaroop <durgaswaroop@gmail.com>
2017-04-03 23:54:26 +0530

    Update Tests
    Updated ConfigTest to create CONFIG file before starting the test.
    Updated Stage test as well

--- src/test/scala/delorean/commands/ConfigTest.scala
+++ src/test/scala/delorean/commands/ConfigTest.scala
@@ -6,14 +6,17 @@
 package delorean.commands
 
 import java.io.{ByteArrayOutputStream, PrintStream}
+import java.nio.file._
 
+import delorean._
 import org.junit.Assert._
-import org.junit.{After, Before, Test}
+import org.junit._
+
+import scala.util.Try
 
 class ConfigTest {
 
     val outContent = new ByteArrayOutputStream()
-
 
     /**
       * This will redirect the output stream into outContent and we can access the print output from it.
@@ -28,9 +31,7 @@ class ConfigTest {
     def cleanUpStreams(): Unit = System.setOut(null)
 
     @Test
-    def configTest: Unit = {
-        val TIMEMACHINE = "test/resources"
-
+    def configTest(): Unit = {
         // Adding a new key value pair to configuration
         Config(List("Marty", "McFly"))
 
@@ -38,4 +39,21 @@ class ConfigTest {
         Config(List("--list"))
         assertEquals("Marty = McFly", outContent.toString.trim)
     }
+}
+
+object ConfigTest {
+    // Create CONFIG file. We also create TIME_MACHINE as we need the parent directory to be present
+    @BeforeClass
+    def setUp(): Unit = {
+        Try(Files.createDirectory(Paths.get(TIME_MACHINE)))
+        Try(Files.createFile(Paths.get(CONFIG)))
+    }
+
+    // Delete CONFIG file and the parent directory.
+    @AfterClass
+    def tearDown(): Unit = {
+        Try(Files.delete(Paths.get(CONFIG)))
+        Try(Files.delete(Paths.get(TIME_MACHINE)))
+    }
+
 }
--- src/test/scala/delorean/commands/StageTest.scala
+++ src/test/scala/delorean/commands/StageTest.scala
@@ -7,9 +7,10 @@ package delorean.commands
 
 import java.nio.file.{Files, Paths}
 
+import delorean.CURRENT_INDICATOR
 import delorean.FileOps._
 import org.junit.Assert._
-import org.junit.{AfterClass, Test}
+import org.junit.{AfterClass, BeforeClass, Test}
 
 class StageTest {
     @Test
@@ -22,5 +23,12 @@ class StageTest {
 }
 
 object StageTest {
+    @BeforeClass
+    def callToRide(): Unit = {
+        // This will make sure it creates all the required files for the test. We are checking for CURRENT_INDICATOR
+        // instead of TIME_MACHINE because .tm could be created by config test
+        if (!Files.exists(Paths.get(CURRENT_INDICATOR))) new delorean.commands.Ride
+    }
+
     @AfterClass def tearDown(): Unit = Files.delete(Paths.get(getTempPitstopFileLocation))
 }
