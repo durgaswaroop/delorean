39bfa75cc4c5734d6c23767edd453115edba268a
DurgaSwaroop <durgaswaroop@gmail.com>
2017-11-05 03:34:20 +0530

    Update CreateTimelineTest
    Added setup and teardown methods that will initiate a delorean repo and delete it at the end of the test.
    So, don't have to save the current timeline anymore. So, deleted it.
    Added some comments as well and another assertion.

--- src/test/scala/delorean/commands/CreateTimelineTest.scala
+++ src/test/scala/delorean/commands/CreateTimelineTest.scala
@@ -5,19 +5,43 @@
 
 package delorean.commands
 
-import delorean.CURRENT_INDICATOR
+import java.io.File
+import java.nio.file.{Files, Paths}
+
+import delorean.{CURRENT_INDICATOR, TIME_MACHINE}
 import delorean.FileOps._
+import org.apache.commons.io.FileUtils
 import org.junit.Assert._
+import org.junit.{AfterClass, BeforeClass, Test}
+
+import scala.util.Try
 
 class CreateTimelineTest {
-    // @Test
+    /**
+      * Checks timeline creation flow by creating a new timeline and  checking if the
+      * current indicator is pointing to the new timeline
+      */
+    @Test
     def createTimelineTest(): Unit = {
-        // Saving the timeline that was there before the test is run
-        val currentTimeline: String = getLinesOfFile(CURRENT_INDICATOR).head
-
         CreateTimeLine("master")
         assertEquals("master", getLinesOfFile(CURRENT_INDICATOR).head)
 
-        writeToFile(CURRENT_INDICATOR, currentTimeline)
+        CreateTimeLine("develop")
+        assertEquals("develop", getLinesOfFile(CURRENT_INDICATOR).head)
+    }
+}
+
+object CreateTimelineTest {
+    @BeforeClass
+    def callToRide(): Unit = {
+        // This will make sure it creates all the required files for the test. We are checking for CURRENT_INDICATOR
+        // instead of TIME_MACHINE because .tm could be created by config test
+        if (!Files.exists(Paths.get(CURRENT_INDICATOR))) new delorean.commands.Ride
+    }
+
+    @AfterClass
+    def tearDown(): Unit = {
+        println("Tearing Down '.tm/' directory created for StageTest")
+        Try(FileUtils.deleteDirectory(new File(TIME_MACHINE)))
     }
 }
